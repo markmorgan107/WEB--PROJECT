bdf4c76248208d1cc296332ba8d55810003e551f
mohamedamr-code <mohamed2402699@miuegypt.edu.eg>
2026-06-04 00:54:24 +0300

    updated quest schema to support users' request for quests

--- models/Quest.js
+++ models/Quest.js
@@ -19,7 +19,7 @@ const QuestSchema = new mongoose.Schema({
     },
     status: {
         type: String,
-        enum: ['active', 'completed', 'pending'],
+        enum: ['active', 'completed', 'pending', 'requested'],
         default: 'pending'
     },
     difficulty: {
