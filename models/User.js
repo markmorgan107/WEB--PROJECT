8bbcaa32b54c87283868a635f6e34eee4ab5aaa3
joyjohnn123 <joy.johnn2020@gmail.com>
2026-06-07 16:28:57 +0300

    created profile picture tracking

--- models/User.js
+++ models/User.js
@@ -42,6 +42,10 @@ const UserSchema = new mongoose.Schema({
     date: {
         type: Date,
         default: Date.now
+    },
+    profilePicture: {
+        type: String,
+        default: ''
     }
 });
 
